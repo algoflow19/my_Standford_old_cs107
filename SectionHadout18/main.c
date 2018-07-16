ffcde68d6b2ed2141d468d01f5ad96cb3058570e
developerkk <2957691307@qq.com>
2018-07-16 15:15:14 +0800

    reget #1

--- SectionHadout18/main.c
+++ SectionHadout18/main.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
 #include<stdlib.h>
-#include<string.h>
+#include"sparsestringarray.h"
 
-typedef struct node node;
-
-struct node{
-  node *next;
-  char headOfString[];
-};
-
-int *doSerializeList(const void * listHead,int *privouseCreated,int totalByteused){
-  if(listHead==NULL) return privouseCreated;
-  *privouseCreated++;
-  int len=strlen((char*)((node*)listHead+1));
-  privouseCreated=realloc(privouseCreated,totalByteused+len+1);
-  strcpy((char*)privouseCreated+totalByteused,(char*)((node*)listHead+1));
-  return doSerializeList ((const void*)*(node**)listHead,privouseCreated,totalByteused+len+1);
+static void CountEmptyPrintNonEmpty(int index, const char *str, void *auxData)
+{
+  if (strcmp(str, "") != 0) {
+      printf("Oooo! Nonempty string at index %d: \"%s\"\n", index, str);
+    } else {
+      (*(int *)auxData)++;
+    }
 }
 
-int *serializeList(const void * listHead){
-  int *SL=malloc(sizeof(int));
-  *SL=0;
-  if(listHead==NULL) return SL;
-  return doSerializeList ((const void*)*(node**)listHead ,SL,sizeof(int) );
-}
 
 int main()
 {
-  char kk[12];
-  char kk2[11]="123445";
-  kk[0]='\0';
-  strcpy(kk,kk2);
-  printf ("%s\n",kk);
+  sparsestringarray ssa;
+  SSANew(&ssa, 70000, 35);
+  SSAInsert(&ssa,33001,"need");
+  SSAInsert(&ssa,58291,"more");
+  SSAInsert(&ssa,33000,"Eye");
+  SSAInsert(&ssa,33000,"I");
+  SSAInsert(&ssa,67899,"cowbell");
+
+  int numEmptyStrings = 0;
+  SSAMap(&ssa, CountEmptyPrintNonEmpty, &numEmptyStrings);
+  printf("%d of the strings were empty strings.\n", numEmptyStrings);
+
+  SSADispose(&ssa);
+  return 0;
+
 }
