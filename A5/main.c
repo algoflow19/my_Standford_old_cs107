1a321d013b0ca6629136b74fae20fed8e25d0f14
developerkk <2957691307@qq.com>
2018-07-17 18:57:49 +0800

    Corrected error

--- A5/main.c
+++ A5/main.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main()
-{
-  printf("Hello World!\n");
-  return 0;
-}
+int main(){
+	char mat[5];
+	mat[0]='1';
+	*(int*)(mat)=4;
+	printf("%ul \n",mat);
+	printf("%ul \n",mat+1);
+
+	printf ("%d \n No bus error",*(int*)(mat));
+	return 0;
+}	
